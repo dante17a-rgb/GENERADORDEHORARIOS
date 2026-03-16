65981f8d8077a37a5947b62fd47ffbc029802b6d
dante17a-rgb <dante.17a@gmail.com>
2026-03-16 16:48:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,8 @@
 import streamlit as st
-import subprocess
+import generadordehorarios
 
 st.title("Generador de Horarios")
 
 if st.button("Run Model"):
-
-    resultado = subprocess.run(
-        ["python3", "generadordehorarios.py"],
-        capture_output=True,
-        text=True
-    )
-
-    st.text(resultado.stdout)
-    st.text(resultado.stderr)
+    st.write("Ejecutando modelo...")
+    generadordehorarios.main()
