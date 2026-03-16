2b2cc0f51b1b0d74929211eec87236d7149aabd4
dante17a-rgb <dante.17a@gmail.com>
2026-03-16 16:42:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,21 @@ import subprocess
 
 st.title("Generador de Horarios")
 
+st.write("Presiona el botón para ejecutar el modelo")
+
 if st.button("Run Model"):
 
+    st.write("Ejecutando...")
+
     resultado = subprocess.run(
-        ["python", "generadordehorarios.py"],
+        ["python3", "generadordehorarios.py"],
         capture_output=True,
         text=True
     )
 
+    st.subheader("Salida del programa")
     st.text(resultado.stdout)
+
+    if resultado.stderr:
+        st.subheader("Errores")
+        st.text(resultado.stderr)
